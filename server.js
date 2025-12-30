b93508d52afba1317851f00ecc29d54127610a53
yikes.. <yikess2468@outlook.com>
2025-12-30 12:27:01 -0500

    Update server.js
    Rewrote "server.js"

--- server.js
+++ server.js
@@ -9,23 +9,37 @@ const io = new Server(server, {
     cors: { origin: "*", methods: ["GET","POST"] }
 });
 
-// Track connected players
+// Track connected players and their usernames
 let players = {};
 
 io.on("connection", (socket) => {
     console.log("Player connected:", socket.id);
-    players[socket.id] = { x: 1, y: 1 };
+
+    // Default username
+    players[socket.id] = { x: 1, y: 1, username: "Player" + Math.floor(Math.random() * 1000) };
+
+    // Set username
+    socket.on("setUsername", (name) => {
+        if (players[socket.id]) {
+            players[socket.id].username = name || players[socket.id].username;
+            io.emit("updatePlayers", players);
+        }
+    });
 
     // Movement
     socket.on("move", (data) => {
-        players[socket.id].x += data.x;
-        players[socket.id].y += data.y;
-        io.emit("updatePlayers", players);
+        if (players[socket.id]) {
+            players[socket.id].x += data.x;
+            players[socket.id].y += data.y;
+            io.emit("updatePlayers", players);
+        }
     });
 
-    // --- Chat system ---
+    // Chat
     socket.on("chatMessage", (msg) => {
-        io.emit("chatMessage", { id: socket.id, text: msg });
+        if (players[socket.id]) {
+            io.emit("chatMessage", { username: players[socket.id].username, text: msg });
+        }
     });
 
     // Disconnect
